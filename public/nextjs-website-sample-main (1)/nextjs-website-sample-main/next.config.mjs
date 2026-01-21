667c72f517c20cce6af11dcae35759c166cc4e0b
takoyaki <n25002@std.it-college.ac.jp>
2026-01-21 11:51:01 +0900

    5章まで完了

--- public/nextjs-website-sample-main (1)/nextjs-website-sample-main/next.config.mjs
+++ public/nextjs-website-sample-main (1)/nextjs-website-sample-main/next.config.mjs
@@ -1,12 +1,10 @@
-import { hostname } from "os";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.microcms-assets.io",
+        protocol: 'https',
+        hostname: 'images.microcms-assets.io',
       },
     ],
   },

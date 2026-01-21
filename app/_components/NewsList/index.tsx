23c7fae285fb541ad5b458ade9bcef4f1664be1a
takoyaki <n25002@std.it-college.ac.jp>
2026-01-21 11:43:27 +0900

    5章まで完了

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -11,6 +11,8 @@ type Props = {
 };
 
 export default function NewsList({ news }: Props) {
+  console.log(news);
+
   if (news.length === 0) {
     return <p>記事がありません</p>;
   }
@@ -19,13 +21,24 @@ export default function NewsList({ news }: Props) {
       {news.map((article) => (
         <li key={article.id} className={styles.list}>
           <Link href={"/news/${article.id}"} className={styles.link}>
-            <Image
-              className={styles.image}
-              src="/no-image.png"
-              alt="No Image"
-              width={1200}
-              height={630}
-            />
+            {article.thumbnail ? (
+              <Image
+                src={article.thumbnail.url}
+                alt=""
+                className={styles.image}
+                width={article.thumbnail.width}
+                height={article.thumbnail.height}
+              />
+            ) : (
+              <Image
+                className={styles.image}
+                src="/no-image.png"
+                alt="No Image"
+                width={1200}
+                height={630}
+              />
+            )}
+
             <dl className={styles.content}>
               <dt className={styles.title}>{article.title}</dt>
               <dd className={styles.meta}>
